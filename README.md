6ec5984944abf1ccfd1b48bbf85589bd62112137
4iwen <luky.bien@gmail.com>
2023-05-29 15:45:33 +0200

    Simple page tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # eCasofond backend
 
 ## Project setup
-```
----
-```
+1. clone the repository
+2. ensure you have jdk 17 installed
+3. ensure you have maven installed
--- src/main/java/com/pwc/ecasofond/ECasofondApplication.java
+++ src/main/java/com/pwc/ecasofond/ECasofondApplication.java
@@ -2,11 +2,24 @@ package com.pwc.ecasofond;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ECasofondApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECasofondApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Index Page <br /> <a href=\"/hello\">Hello</a>";
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World! <br /> <a href=\"/\">Back</a>";
 	}
 }
